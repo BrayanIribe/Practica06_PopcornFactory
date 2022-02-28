94d195d7f86b201d320e3bd5b9c86b4858107aa5
Brayan Iribe <brayaniribe@gmail.com>
2022-02-28 00:07:20 -0700

    Build inicial PopcornFactory

---
+++ app/src/main/java/com/example/popcornfactory/DetallePelicula.kt
@@ -0,0 +1,17 @@
+package com.example.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detalle_pelicula.*
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pelicula)
+        val bundle = intent.extras ?: return
+
+        iv_imagen_pelicula.setImageResource(bundle.getInt("header"))
+        tv_TituloPelicula.setText(bundle.getString("nombre"))
+        tv_DescPelicula.setText(bundle.getString("sinopsis"))
+    }
+}
