4e7e602eba8c42bcba3eaca78a8e9def05581e90
Brayan Iribe <brayaniribe@gmail.com>
2022-03-08 23:57:13 -0700

    Agregado selección de asientos y vistas

--- app/src/main/java/com/example/popcornfactory/DetallePelicula.kt
+++ app/src/main/java/com/example/popcornfactory/DetallePelicula.kt
@@ -1,5 +1,6 @@
 package com.example.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_detalle_pelicula.*
@@ -13,5 +14,15 @@ class DetallePelicula : AppCompatActivity() {
         iv_imagen_pelicula.setImageResource(bundle.getInt("header"))
         tv_TituloPelicula.setText(bundle.getString("nombre"))
         tv_DescPelicula.setText(bundle.getString("sinopsis"))
+
+        var id = bundle.getInt("pos")
+        var titulo = bundle.getString("titulo")!!
+
+        buyTickets.setOnClickListener {
+            val intent: Intent = Intent(this, SeatSelection::class.java)
+            intent.putExtra("id", id)
+            intent.putExtra("title", titulo)
+            startActivity(intent)
+        }
     }
 }
